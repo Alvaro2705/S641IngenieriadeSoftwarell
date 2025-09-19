d3862e0cbf3bf240b3e07e6e349dbf6224217fd9
Alvaro2705 <alvarostevenfarrufia@gmail.com>
2025-09-19 15:08:48 +0000

    Creacion de los patrones builder

---
+++ PatronBuilderS641/Reader.java
@@ -0,0 +1,32 @@
+public class Reader {
+
+    private String element;
+    private Converter converter;
+
+    public Reader(String element, Converter converter) {
+        this.element = element;
+        this.converter = converter;
+    }
+
+    // Metodos
+    public void parseInput() {
+        switch (element) {
+
+            case "LINE":
+                converter.makeLine();
+                break;
+
+            case "PRAGRAPH":
+                converter.makeParagraph();
+                break;
+
+            case "TABLE":
+                converter.makeTable();
+                break;
+
+            default:
+                System.out.println("Opcion no valida");
+                break;
+        }
+    }
+}
